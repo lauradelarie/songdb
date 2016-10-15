e1e4d03fb1a892077fb95b23cdfcbe0da494bc24
lauradelar <lrdela.rie@gmail.com>
2016-10-15 11:45:42 +0200

    add image to artist show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015083941) do
+ActiveRecord::Schema.define(version: 20161015093939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20161015083941) do
     t.string   "image"
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.integer  "artist_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_photos_on_artist_id", using: :btree
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string   "title"
     t.string   "album"
@@ -34,5 +42,6 @@ ActiveRecord::Schema.define(version: 20161015083941) do
     t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
 
+  add_foreign_key "photos", "artists"
   add_foreign_key "songs", "artists"
 end
