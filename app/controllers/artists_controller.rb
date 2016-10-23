4723f9c59ae611629427ac9c4e8da4c92f9bb7c6
lauradelar <lrdela.rie@gmail.com>
2016-10-23 09:51:03 +0200

    clean up controllers

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,25 +1,13 @@
 class ArtistsController < ApplicationController
 
   def index
-    # @artists = Artist.search(params[:search])
+    @artists = Artist.search(params[:search])
     @artists = Artist.all
     @songs = Song.all
   end
 
   def show
-    @song = Song.new
-
     @artist = Artist.find(params[:id])
-    @songs = Song.all
-  end
-
-  def create
-    @artist = Artist.find(song_params[:artist_id])
-    @song = @artist.songs.create(song_params)
-
-      if @song.save
-        redirect_to artist_songs_path, notice: "You added a song!"
-      end
   end
 
 
@@ -32,13 +20,6 @@ class ArtistsController < ApplicationController
   def created_at
     @artists = Artist.created_at
     render action: :index
-  end
-
-
-private
-
-  def song_params
-    params.require(:song).permit(:title, :artist_id)
   end
 
 end
